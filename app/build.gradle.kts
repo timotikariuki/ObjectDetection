008bec6f94bd1e4d49a70f043f9467425dd6d7dc
timotikariuki <muhorotimothy5@gmail.com>
2024-07-10 23:43:56 +0800

    added dependency to download models.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,14 @@
+import de.undercouch.gradle.tasks.download.Download
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("de.undercouch.download")
 }
+
+// Define ASSET_DIR and TEST_ASSETS_DIR directly in your build.gradle.kts file
+val ASSET_DIR = "${projectDir}/src/main/assets"
+val TEST_ASSETS_DIR = "${projectDir}/src/androidTest/assets"
 
 android {
     namespace = "com.test.objectdetection"
@@ -48,6 +55,15 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    androidResources  {
+        noCompress.add("tflite")
+    }
+
+    packagingOptions {
+        resources {
+            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+        }
+    }
 }
 
 dependencies {
@@ -78,4 +94,40 @@ dependencies {
     debugImplementation(libs.ui.test.manifest)
     androidTestImplementation(libs.ui.test.junit4)
 
+}
+
+// Download default models; if you wish to use your own models then
+// place them in the "assets" directory and comment out this line.
+tasks.register<Download>("downloadModelFile") {
+    src("https://storage.googleapis.com/download.tensorflow.org/models/tflite/task_library/object_detection/android_play_services/lite-model_ssd_mobilenet_v1_1_metadata_2.tflite")
+    dest(file("$ASSET_DIR/mobilenetv1.tflite"))
+    overwrite(false)
+}
+
+tasks.register<Download>("downloadModelFile0") {
+    src("https://storage.googleapis.com/download.tensorflow.org/models/tflite/task_library/object_detection/android_play_services/lite-model_efficientdet_lite0_detection_metadata_1.tflite")
+    dest(file("$ASSET_DIR/efficientdet-lite0.tflite"))
+    overwrite(false)
+}
+
+tasks.register<Download>("downloadModelFile1") {
+    src("https://storage.googleapis.com/download.tensorflow.org/models/tflite/task_library/object_detection/android_play_services/lite-model_efficientdet_lite1_detection_metadata_1.tflite")
+    dest(file("$ASSET_DIR/efficientdet-lite1.tflite"))
+    overwrite(false)
+}
+
+tasks.register<Download>("downloadModelFile2") {
+    src("https://storage.googleapis.com/download.tensorflow.org/models/tflite/task_library/object_detection/android_play_services/lite-model_efficientdet_lite2_detection_metadata_1.tflite")
+    dest(file("$ASSET_DIR/efficientdet-lite2.tflite"))
+    overwrite(false)
+}
+
+tasks.register<Copy>("copyTestModel") {
+    dependsOn("downloadModelFile")
+    from(file("$ASSET_DIR/mobilenetv1.tflite"))
+    into(file(TEST_ASSETS_DIR))
+}
+
+tasks.named("preBuild") {
+    dependsOn("downloadModelFile", "downloadModelFile0", "downloadModelFile1", "downloadModelFile2", "copyTestModel")
 }
--- app/src/main/java/com/test/objectdetection/MainActivity.kt
+++ app/src/main/java/com/test/objectdetection/MainActivity.kt
@@ -3,9 +3,6 @@ package com.test.objectdetection
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.test.objectdetection.databinding.ActivityMainBinding
 
 /**
@@ -15,7 +12,6 @@ import com.test.objectdetection.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var activityMainBinding: ActivityMainBinding
-    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
